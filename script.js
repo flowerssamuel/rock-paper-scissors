dd27d6b6ae797fb2c7316def5657c52fbf3877f5
flowerssamuel <samuelflowers29@gmail.com>
2026-01-30 16:44:26 +0000

    Create function getComputerChoice

---
+++ script.js
@@ -0,0 +1,11 @@
+let getComputerChoice = () => {
+    let randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice;
+    if (randomNumber === 0) {
+        computerChoice = "Rock";
+    } else if (randomNumber === 1) {
+        computerChoice = "Paper";
+    } else 
+        computerChoice = "Scissors";
+    return computerChoice;
+};
