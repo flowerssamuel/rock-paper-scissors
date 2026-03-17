abf7cea007c9ec36b32f7acb4ef3759276e2f46c
flowerssamuel <samuelflowers29@gmail.com>
2026-03-17 04:26:01 +0100

    Replace console.logs in the playRound function with DOM methods

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const game = document.querySelector("#game");
+const results = document.querySelector("#results");
+let playerScore = document.querySelector("#playerScore");
+let comScore = document.querySelector("#comScore");
+
 let getComputerChoice = () => {
     let randomNumber = Math.floor(Math.random() * 3);
     let computerChoice;
@@ -11,33 +16,36 @@ let getComputerChoice = () => {
 };
 
 let getHumanChoice = () => {
+    let humanChoice = prompt("Enter Rock, Paper or Scissors");
+    humanChoice = humanChoice.toUpperCase();
     return humanChoice;
 };
 
 let playGame = () => {
     let playRound = (humanChoice, computerChoice) => {
         if (humanChoice === "Rock" && computerChoice === "Scissors") {
-            console.log("You win! Rock beats Scissors");
+            results.textContent = "You win! Rock beats Scissors";
             ++humanScore;
         } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-            console.log("You win! Paper beats Rock");
+            results.textContent = "You win! Paper beats Rock";
             ++humanScore;
         } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-            console.log("You win! Scissors beats Paper");
+            results.textContent = "You win! Scissors beats Paper";
             ++humanScore;
         } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
-            console.log("You lose! Rock beats Scissors");
+            results.textContent = "You lose! Rock beats Scissors";
             ++computerScore;
         } else if (computerChoice === "Paper" && humanChoice === "Rock") {
-            console.log("You lose! Paper beats Rock");
+            results.textContent = "You lose! Paper beats Rock";
             ++computerScore; 
         } else if (computerChoice === "Scissors" && humanChoice === "Paper") {
-            console.log("You lose! Scissors beats Paper");
+            results.textContent = "You lose! Scissors beats Paper";
             ++computerScore;
         } else if (humanChoice === computerChoice) {
-            console.log("It's a draw!");
+            results.textContent = "It's a draw!";
         }
     };
+
     console.log(`The Player's Score is ${humanScore}`);
     console.log(`The Computer's Score is ${computerScore}`);
 };
